b8091096dcda830b32eef429edd3c0b9dfd564f3
Asadullah Ilyas <the.elder_wand@yahoo.com>
2023-01-05 13:18:59 +0500

    ErrorResponse.kt added

---
+++ OpenAI-Kotlin/src/main/kotlin/com/asadullah/api/response/ErrorResponse.kt
@@ -0,0 +1,22 @@
+package com.asadullah.api.response
+
+import com.squareup.moshi.Json
+import com.squareup.moshi.JsonClass
+
+@JsonClass(generateAdapter = true)
+data class ErrorResponse(
+    @Json(name = "error")
+    val error: Error
+) {
+    @JsonClass(generateAdapter = true)
+    data class Error(
+        @Json(name = "code")
+        val code: String,
+        @Json(name = "message")
+        val message: String,
+        @Json(name = "param")
+        val `param`: Any,
+        @Json(name = "type")
+        val type: String
+    )
+}
